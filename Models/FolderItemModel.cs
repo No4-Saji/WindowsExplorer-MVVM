a89065f025a633428820156157b73948abc28e17
r.saji <r.saji@no4.co.jp>
2024-05-27 11:00:29 +0900

    FolderItemModelにフォルダのパス・名前、アイコンのパスを保持させる

--- Models/FolderItemModel.cs
+++ Models/FolderItemModel.cs
@@ -8,15 +8,37 @@ using System.Threading.Tasks;
 namespace WindowsExplorer.Models
 {
     /// <summary>
-    /// ディレクトリ関連の処理
+    /// フォルダー関連の処理
     /// </summary>
     /// <param name="directoryInfo">　ディレクトリ　</param>
     internal class FolderItemModel
     {
+        #region プロパティ
+        
+        /// <summary>
+        /// iconのフォルダのパス
+        /// </summary>
+        public static string IconUri { get; } = @"\Resources\Folder.ico";
+        /// <summary>
+        /// 取得したフォルダの名前
+        /// </summary>
+        public string FolderName { get; set; }
 
+        /// <summary>
+        /// 取得したフォルダのパス
+        /// </summary>
+        public string FolderPath { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// フォルダのパスと名前を保持する。
+        /// </summary>
+        /// <param name="directoryInfo"> NavigationItemViewModelから渡されたディレクトリ　</param>
         public FolderItemModel(DirectoryInfo directoryInfo)
         {
-
+            FolderName = directoryInfo.Name;
+            FolderPath = directoryInfo.FullName;
         }
     }
 }
